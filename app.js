04e0e431ed40bc55a31ca440ed4faa9d4c44d2e9
hermanwooooo <hermanwooooo@users.noreply.github.com>
2020-10-25 01:41:54 -0400

    newchanges

--- app.js
+++ app.js
@@ -23,50 +23,40 @@
     const cityContainer = document.getElementById("sections");
     //get all city sections inside the seciton container
     const cities = cityContainer.getElementsByClassName("city section");
-    const header = document.querySelector("nav");
-    let maxHeight = 0
+    const header = document.querySelector("ul");
     for (const city of cities){
-        const height = city.offsetHeight;
-        //get the id of the city section
-        const cityTag = city.id;
-        //get the inner text of the city section
-        const cityName = city.getElementsByClassName("city name")[0].innerText;
-        //create a link element and give it the inner text, class and href from city section
-        const cityLink = document.createElement('a')
+        const cityTag = city.id; //get the id of the city section
+        const cityName = city.getElementsByClassName("city name")[0].innerText; //get the inner text of the city section
+        const cityItem = document.createElement('li'); 
+        const cityLink = document.createElement('a');//create a link element and give it the inner text, class and href from city section
         cityLink.textContent = cityName;
         cityLink.classList.add("link");
         cityLink.href="#"+cityTag;
-        header.appendChild(cityLink);
-        maxHeight += city.offsetHeight;
+        cityItem.appendChild(cityLink);
+        header.appendChild(cityItem);
     }
 
-
-    
+//3. Swtich links to active state depending on where the user has scrolled to on the page
+    //triggers anytime user scrolls    
     window.addEventListener('scroll',function(){
+        //get the HTMLcollection of links
         const links = header.getElementsByClassName("link");
+        //parameters to know where the user is in the broser
         let height = window.scrollY;
         let lowerLimit = 0;
-        let upperLimit = 1000;
+        let upperLimit = 1000; //this is the height of the first header div, in future code should also be found through the property of the first div
 
-
+        //For loop that iterates through cities, but chose not to use for Of this time to get an interator to locate the link
         for (let i = 0; i<cities.length;i++){
             let length = cities[i].offsetHeight;
-            lowerLimit = upperLimit;
-            upperLimit = upperLimit + length;
-            const link = links[i];
+            lowerLimit = upperLimit; //the lower limit will always become what the upperlimt was
+            upperLimit = upperLimit + length; //the upper limit will be dynamically increased based on the heigh of each section
+            const link = links[i]; // grab the link and determine if any action is required to be taken on it
             if (height >= lowerLimit && height < upperLimit){
-                link.classList.add("active");
-
+                link.classList.add("active");//if the current height at the top of the page falls inbetween the current limits, set the link to active.
             }
             else{
                 link.classList.remove("active");
             }
-
         }
-
-
-
-
-       
-        
     });
